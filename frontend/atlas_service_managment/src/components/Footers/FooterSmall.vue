b26808e6d2d7110aa6e1b5cd461dc79b7c45160d
Aleks Milchov <alexmilchov02@gmail.com>
2020-12-17 14:19:33 +0200

    First time frontend files commit

---
+++ frontend/atlas_service_managment/src/components/Footers/FooterSmall.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer
+    class="pb-6"
+    :class="[absolute ? 'absolute w-full bottom-0 bg-gray-900' : 'relative']"
+  >
+    <div class="container mx-auto px-4">
+      <hr class="mb-6 border-b-1 border-gray-700" />
+      <div
+        class="flex flex-wrap items-center md:justify-between justify-center"
+      >
+        <div class="w-full md:w-10/12 px-4 mx-auto text-center">
+          <div class="text-sm text-white font-semibold py-1">
+            NOIT {{ date }} -  Category "Distributed Web Applications" | Aleks Milchov and David Ivanov | 
+            <a
+              href="https://pgi-pernik.bg-schools.com/"
+              target="_blank"
+              class="text-white hover:text-gray-300"
+            >
+              PGI Pernik
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    };
+  },
+  props: {
+    absolute: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
---
+++ frontend/atlas_service_managment/src/views/Landing.vue
@@ -0,0 +1,422 @@
+<template>
+  <div>
+    <navbar />
+    <main>
+      <div
+        class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75"
+      >
+        <div
+          class="absolute top-0 w-full h-full bg-center bg-cover"
+          style="
+            background-image: url('https://i0.wp.com/www.hisour.com/wp-content/uploads/2019/04/ATLAS-experiment-CERN-Geneva-Switzerland.jpg?fit=960%2C640&ssl=1&resize=1280%2C720');
+          "
+        >
+          <span
+            id="blackOverlay"
+            class="w-full h-full absolute opacity-75 bg-black"
+          ></span>
+        </div>
+        <div class="container relative mx-auto">
+          <div class="items-center flex flex-wrap">
+            <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+              <div class="pr-12">
+                <h1 class="text-white font-semibold text-5xl">
+                  ATLAS Experiment
+                </h1>
+                <p class="mt-4 text-lg text-gray-300">
+                  Project aim is to monitor the distribution of the calculations of the experimental ATLAS in the CENTER 
+                  and reviewing the current state and load of the computer resources, 
+                  how many CPUs are currently available. The whole process is visualized through interactive diagrams.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+          style="transform: translateZ(0);"
+        >
+          <svg
+            class="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              class="text-gray-300 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+      </div>
+
+      <section class="pb-20 bg-gray-300 -mt-24">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap">
+            <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+              <div
+                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+              >
+                <div class="px-4 py-5 flex-auto">
+                  <div
+                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400"
+                  >
+                    <i class="fas fa-award"></i>
+                  </div>
+                  <h6 class="text-xl font-semibold">"What is ADC?</h6>
+                  <p class="mt-2 mb-4 text-gray-600">
+                    ADC is ATLAS Distributed Computing - the distributed computation system of the ATLAS experiment.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="w-full md:w-4/12 px-4 text-center">
+              <div
+                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+              >
+                <div class="px-4 py-5 flex-auto">
+                  <div
+                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400"
+                  >
+                    <i class="fas fa-retweet"></i>
+                  </div>
+                  <h6 class="text-xl font-semibold">What is CERN?</h6>
+                  <p class="mt-2 mb-4 text-gray-600">
+                    CERN is the world's largest particle physics laboratory. 
+                    It is one of the most respected centres for scientific research. 
+                    Its business is fundamental physics: finding out what the universe is made of and how it works.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div class="pt-6 w-full md:w-4/12 px-4 text-center">
+              <div
+                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg"
+              >
+                <div class="px-4 py-5 flex-auto">
+                  <div
+                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400"
+                  >
+                    <i class="fas fa-fingerprint"></i>
+                  </div>
+                  <h6 class="text-xl font-semibold">What is BigPanDA?</h6>
+                  <p class="mt-2 mb-4 text-gray-600">
+                     BigPanDA is a an IT systems management platform that aggregates and correlates IT alerts to 
+                     create high-level IT incidents. BigPanDA is monitoring the current status of tasks in ATLAS.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap items-center mt-32">
+            <div class="w-full md:w-5/12 px-4 mr-auto ml-auto">
+              <div
+                class="text-gray-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-gray-100"
+              >
+                <i class="fas fa-atom text-xl"></i>
+              </div>
+              <h3 class="text-3xl mb-2 font-semibold leading-normal">
+                Working with raw data from BigPanDA!
+              </h3>
+              <p
+                class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700"
+              >
+                PanDA was designed to have the flexibility to adapt to emerging computing 
+                technologies in processing, storage, networking and distributed computing 
+                middleware. 
+              </p>
+              <p
+                class="text-lg font-light leading-relaxed mt-0 mb-4 text-gray-700"
+              >
+                The interest in PanDA by other big data sciences brought the primary motivation to 
+                generalize PanDA, aka the BigPanDA project, providing location transparency of 
+                processing and data management, for High Energy Physics community and other 
+                data-intensive sciences, and a wider exascale community. The BigPanDA project 
+                has been active since September 2012.
+              </p>
+              <a 
+              href="https://atlas.cern/" 
+              class="font-bold text-gray-800 mt-8"
+              target="_blank">
+                Check Atlas Experiment!
+              </a>
+            </div>
+
+            <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
+              <div
+                class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-blue-500"
+              >
+                <img
+                  alt="..."
+                  src="https://atlas.cern/sites/atlas-public.web.cern.ch/files/detector-discover.jpg"
+                  class="w-full align-middle rounded-t-lg"
+                />
+                <blockquote class="relative p-8 mb-4">
+                  <svg
+                    preserveAspectRatio="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 583 95"
+                    class="absolute left-0 w-full block h-95-px -top-94-px"
+                  >
+                    <polygon
+                      points="-30,95 583,95 583,65"
+                      class="text-blue-600 fill-current"
+                    ></polygon>
+                  </svg>
+                  <h4 class="text-xl font-bold text-white">
+                    About the ATLAS Experiment
+                  </h4>
+                  <p class="text-md font-light mt-2 text-white">
+                    ATLAS is one of the four major experiments at the Large Hadron Collider (LHC) at CERN. 
+                    It is a general-purpose particle physics experiment run by an international collaboration and,
+                    together with CMS, is designed to exploit the full discovery potential and the huge range of physics 
+                    opportunities that the LHC provides.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="relative py-20">
+        <div
+          class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
+          style="transform: translateZ(0);"
+        >
+          <svg
+            class="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              class="text-white fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+      </section>
+
+      <section class="pb-48">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap justify-center text-center mb-24">
+            <div class="w-full lg:w-6/12 px-4">
+              <h2 class="text-4xl font-semibold">Here are our heroes</h2>
+              <p class="text-lg leading-relaxed m-4 text-gray-600">
+                The people who developed the system for analyzing and visualizing the calculations from the ATLAS experiment.
+              </p>
+            </div>
+          </div>
+          <div class="flex flex-wrap">
+            <div class="w-full md:w-6/12 lg:w-6/12 lg:mb-0 mb-12 px-2">
+              <div class="px-6">
+                <img
+                  alt="..."
+                  :src="team1"
+                  class="shadow-lg rounded-full mx-auto max-w-120-px"
+                />
+                <div class="pt-6 text-center">
+                  <h5 class="text-xl font-bold">Aleks Milchov</h5>
+                  <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
+                    Web Developer
+                  </p>
+                  <div class="mt-6">
+                    <button
+                      class="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                      type="button"
+                    >
+                      <i class="fab fa-twitter"></i>
+                    </button>
+                    <button
+                      class="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                      type="button"
+                    >
+                      <i class="fab fa-facebook-f"></i>
+                    </button>
+                    <button
+                      class="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                      type="button"
+                    >
+                      <i class="fab fa-dribbble"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-6/12 lg:w-6/12 lg:mb-0 mb-12 px-2">
+              <div class="px-6">
+                <img
+                  alt="..."
+                  :src="team2"
+                  class="shadow-lg rounded-full mx-auto max-w-120-px"
+                />
+                <div class="pt-6 text-center">
+                  <h5 class="text-xl font-bold">David Ivanov</h5>
+                  <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
+                    Backend Developer
+                  </p>
+                  <div class="mt-6">
+                    <button
+                      class="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                      type="button"
+                    >
+                      <i class="fab fa-google"></i>
+                    </button>
+                    <button
+                      class="bg-blue-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
+                      type="button"
+                    >
+                      <i class="fab fa-facebook-f"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="pb-20 relative block bg-gray-900">
+        <div
+          class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
+          style="transform: translateZ(0);"
+        >
+          <svg
+            class="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              class="text-gray-900 fill-current"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
+
+        <div class="container mx-auto px-4 lg:pt-24 lg:pb-64">
+          <div class="flex flex-wrap text-center justify-center">
+            <div class="w-full lg:w-6/12 px-4">
+              <h2 class="text-4xl font-semibold text-white">
+                Do you have any questions?
+              </h2>
+              <p class="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
+                Don't hesitate to contact us when you have 
+                any questions related to our web application!
+              </p>
+            </div>
+          </div>
+          
+        </div>
+      </section>
+      <section class="relative block py-24 lg:pt-0 bg-gray-900">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap justify-center lg:-mt-64 -mt-48">
+            <div class="w-full lg:w-6/12 px-4">
+              <div
+                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300"
+              >
+                <div class="flex-auto p-5 lg:p-10">
+                  <h4 class="text-2xl font-semibold">
+                    Contact us!
+                  </h4>
+                  <p class="leading-relaxed mt-1 mb-4 text-gray-600">
+                    Complete this form and we will get back to you in 24 hours.
+                  </p>
+                  <div class="relative w-full mb-3 mt-8">
+                    <label
+                      class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                      htmlFor="full-name"
+                    >
+                      Full Name
+                    </label>
+                    <input
+                      type="text"
+                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+                      placeholder="Full Name"
+                    />
+                  </div>
+
+                  <div class="relative w-full mb-3">
+                    <label
+                      class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                      htmlFor="email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+                      placeholder="Email"
+                    />
+                  </div>
+
+                  <div class="relative w-full mb-3">
+                    <label
+                      class="block uppercase text-gray-700 text-xs font-bold mb-2"
+                      htmlFor="message"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      rows="4"
+                      cols="80"
+                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                      placeholder="Type a message..."
+                    />
+                  </div>
+                  <div class="text-center mt-6">
+                    <button
+                      class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                    >
+                      Send Message
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer-component />
+  </div>
+</template>
+<script>
+import Navbar from "@/components/Navbars/AuthNavbar.vue";
+import FooterComponent from "@/components/Footers/Footer.vue";
+
+import team1 from "@/assets/img/team-1.jpg";
+import team2 from "@/assets/img/team-2.jpg";
+import team3 from "@/assets/img/team-3-800x800.jpg";
+import team4 from "@/assets/img/team-4-470x470.png";
+
+export default {
+  data() {
+    return {
+      team1,
+      team2,
+      team3,
+      team4,
+    };
+  },
+  components: {
+    Navbar,
+    FooterComponent,
+  },
+};
+</script>

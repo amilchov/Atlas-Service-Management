13ecd6bb13b2462b68daa37533a05da26db8cf07
DESKTOP-IB5E4D7\David <david4obgg1@gmail.com>
2021-03-21 14:25:02 +0200

    Add brand new folder about Administrator stuff.

---
+++ backend/app/Http/Api/Administrator/Controllers/AdministratorController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Api\Administrator\Controllers;
+
+use App\Http\Api\Administrator\Services\AdministratorService;
+use App\Http\Api\User\Requests\UpdateRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+/**
+|--------------------------------------------------------------------------
+| Administrator Controller
+|--------------------------------------------------------------------------
+|
+| This class is a type of controller, in which we have
+| the basic administrator data from the Administrator Service class.
+|
+| @author David Ivanov <david4obgg1@gmail.com>
+ */
+class AdministratorController extends Controller
+{
+    /**
+     * Initializing the instance of Administrator Service class.
+     *
+     * @var AdministratorService
+     */
+    private AdministratorService $administratorService;
+
+    /**
+     * AdministratorController constructor.
+     *
+     * @param AdministratorService $administratorService
+     */
+    public function __construct(AdministratorService $administratorService)
+    {
+        $this->administratorService = $administratorService;
+    }
+
+    /**
+     * Update the user with the specific id.
+     *
+     * @param int $id
+     * @param UpdateRequest $request
+     * @return JsonResponse
+     */
+    public function updateUser(int $id, UpdateRequest $request): JsonResponse
+    {
+        return $this->administratorService->updateUser($id, $request);
+    }
+
+    /**
+     * Delete the user with the specific id.
+     *
+     * @param int $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function destroyUser(int $id, Request $request): JsonResponse
+    {
+        return $this->administratorService->deleteUser($id, $request);
+    }
+}
---
+++ backend/app/Http/Api/Administrator/Services/AdministratorService.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Api\Administrator\Services;
+
+use App\Http\Traits\UserManagement;
+use App\Http\Api\User\Interfaces\UserRepositoryInterface;
+use App\Http\Api\User\Requests\UpdateRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+/**
+|--------------------------------------------------------------------------
+| Administrator Service
+|--------------------------------------------------------------------------
+|
+| This class is a type of service, in which we are
+| getting the full data about administrator stuff.
+|
+| @author David Ivanov <david4obgg1@gmail.com>
+ */
+class AdministratorService
+{
+    use UserManagement;
+
+    /**
+     * Initializing the instance of User Repository interface.
+     *
+     * @var UserRepositoryInterface
+     */
+    private UserRepositoryInterface $userRepository;
+
+    /**
+     * AdministratorService constructor.
+     *
+     * @param UserRepositoryInterface $userRepository
+     */
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * Update the user with his credentials.
+     *
+     * @param int $id
+     * @param UpdateRequest $request
+     * @return JsonResponse
+     */
+    public function updateUser(int $id, UpdateRequest $request): JsonResponse
+    {
+        $this->userRepository->findByToken($request);
+
+        $user = $this->userRepository->findById($id);
+
+        return $this->changeUser($user, $request);
+    }
+
+    /**
+     * Delete the user with his credentials.
+     *
+     * @param int $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function deleteUser(int $id, Request $request): JsonResponse
+    {
+        $this->userRepository->findByToken($request);
+
+        $user = $this->userRepository->findById($id);
+
+        return $this->removeUser($user);
+    }
+}
